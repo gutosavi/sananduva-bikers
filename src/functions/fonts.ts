49a2fe162135e390b5108029b8d163e7ad7e85d7
Gustavo Savi <gutosavi@hotmail.com>
2026-08-17 12:49:52 -0300

    fix: reorganiza a pasta fonts

--- src/functions/fonts.ts
+++ src/functions/fonts.ts
@@ -6,17 +6,17 @@ export const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 export const neueMachina = localFont({
   src: [
     {
-      path: "../app/fonts/NeueMachina-Light.woff2",
+      path: "../app/fonts/neue-machina/NeueMachina-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../app/fonts/NeueMachina-Regular.woff2",
+      path: "../app/fonts/neue-machina/NeueMachina-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../app/fonts/NeueMachina-Ultrabold.woff2",
+      path: "../app/fonts/neue-machina/NeueMachina-Ultrabold.woff2",
       weight: "900",
       style: "normal",
     },
